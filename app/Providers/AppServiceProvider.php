d343d3e2284bda1deb040f2feea052cec9e4a728
antonio.souza <129562096+antoniosouza977@users.noreply.github.com>
2024-04-06 00:37:07 -0400

    feat: back button to previous url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        Inertia::share([
+            'previousUrl' => function () {
+                if (url()->previous() === route('login') || url()->previous() === '') return 'empty';
+
+                if (url()->previous() === url()->current()) {
+                    return dirname(url()->previous());
+                }
+
+                return url()->previous();
+            },
+        ]);
     }
 
     /**
---
+++ resources/js/Components/Buttons/BackButton.vue
@@ -0,0 +1,19 @@
+<script>
+import {useToast} from "vue-toastification";
+import {router} from "@inertiajs/vue3";
+
+export default {
+    name: "BackButton",
+    methods: {
+        bacK() {
+            if (this.$page.props.previousUrl === 'empty') return useToast().warning('Pagina Anterior inválida!');
+            return router.get(this.$page.props.previousUrl)
+        }
+    }
+}
+</script>
+
+<template>
+    <Button @click.prevent="bacK" icon="pi pi-arrow-left" severity="info" rounded/>
+</template>
+
